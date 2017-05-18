ef087584f98450800ff3326a407a0f125db4e0df
florin marinescu <marinescu.florin.alexandru@gmail.com>
2017-05-19 01:06:41 +0300

    Refactor template for event list component

--- app/events/events-list.component.ts
+++ app/events/events-list.component.ts
@@ -6,7 +6,11 @@ import { Component } from '@angular/core'
 <div>
     <h1>Upcoming Angular 2 Events</h1>
     <hr>
-    <event-thumbnail *ngFor="let event of events"  [event]="event"></event-thumbnail>
+    <div class="row">
+      <div *ngFor="let event of events" class="col-md-5">
+        <event-thumbnail  [event]="event"></event-thumbnail>
+      </div>
+    </div>
 </div>`
 })
 export class EventsListComponent{
